618c9c038e893442453c5e748569bc16e5116a24
Andrew McCormack <andrewm@cyber-duck.co.uk>
2015-08-02 01:40:44 +0100

    add traffic model comments

--- code/model/BlockModel.php
+++ code/model/BlockModel.php
@@ -9,6 +9,9 @@
  **/
 class BlockModel extends DataObject {
 
+	/**
+	 * @static array $db databse columns which contain blocked traffic
+	 **/
 	private static $db = array(
 		'description' => 'Varchar(255)',
 		'ip' 	      => 'Varchar(255)',
@@ -18,8 +21,15 @@ class BlockModel extends DataObject {
 		'referer'     => 'Varchar(255)',
 		);
 
+	/**
+	 * @static string $singular_name singular admin name
+	 **/
 	private static $singular_name = 'Blocked';
 
+	/**
+	 * create the CMS fields where we can enter any blocked IPs etc
+	 * @return void
+	 **/
 	public function getCMSFields()
 	{
 		return new FieldList(
@@ -35,15 +45,33 @@ class BlockModel extends DataObject {
 
 class BlockAdmin extends ModelAdmin {
 
+	/**
+	 * @static array $managed_models this class manages our BlockModel
+	 **/
 	private static $managed_models = array('BlockModel');
 
+	/**
+	 * @static string $url_segment the CMS URL segment
+	 **/
 	private static $url_segment = 'blocked';
 
+	/**
+	 * @static string $menu_title the CMS menu link text
+	 **/
 	private static $menu_title = 'Blocked Users';
 
+	/**
+	 * @static string $menu_icon the CMS menu icon
+	 **/
 	private static $menu_icon = 'blacklist/images/menu-icons/16x16/block.png';
 
+	/**
+	 * @static string $page_length the number of records to show per page in the CMS
+	 **/
 	private static $page_length = 100;
 
+	/**
+	 * @static string $menu_priority we set this very high so this will be the last CMS menu item
+	 **/
 	private static $menu_priority = -100;
 }
