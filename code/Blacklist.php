52af309f0bafe895edaa582d14069bb45c89629a
Andrew McCormack <andrewm@cyber-duck.co.uk>
2015-08-03 00:49:23 +0100

    update blacklist class

--- code/Blacklist.php
+++ code/Blacklist.php
@@ -1,39 +1,10 @@
 <?php
-/* 
- *
- * Copyright (c) 2015, Andrew Mc Cormack <andrewm@cyber-duck.co.uk>.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *
- *   * Neither the name of Andrew Mc Cormack nor the names of his
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+/**
  * Blacklist
+ * The core of the Blacklist module here does very little related to the logic 
+ * of the module. Instead its responsible for setting and getting dependencies. 
+ * Through this approach we allow the easy extension of the module itself and 
+ * leave it open for easy integration of new features in future.
  *
  * @package silverstripe-blacklist
  * @license BSD License http://www.silverstripe.org/bsd-license
@@ -145,7 +116,7 @@ class Blacklist {
 				foreach (explode(',', $_SERVER[$key]) as $ip) :
 					$ip = trim($ip);
 
-					// we use filter_var to validate we have have a proper IP address
+					// validate we have have a proper IP address
 					if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE 
 						| FILTER_FLAG_NO_RES_RANGE) !== false) :
 
@@ -185,7 +156,7 @@ class Blacklist {
 	 **/
 	public function run()
 	{
-		// initiate a traffic object for logging and pass in our user information
+		// initiate a traffic object for logging and pass in our user info
 		$this->traffic = new Traffic(
 			$this->userIP,
 			$this->userHost,
